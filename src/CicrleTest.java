1d2ae02585aa1ad6595cb74e7bfb9337775552eb
hung060798 <hungpppp122gmail.com>
2021-06-11 21:59:47 +0700

    th

---
+++ src/CicrleTest.java
@@ -0,0 +1,12 @@
+public class CicrleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        circle = new Circle(3.5);
+        System.out.println(circle);
+
+        circle = new Circle(3.5, "green",false);
+        System.out.println(circle);
+    }
+}
